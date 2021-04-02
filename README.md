ffe918058cbac1871da4fd1b4fb39d45c81f3167
Jae Hward <howardjake9512@gmail.com>
2021-04-02 16:17:55 -0400

    allowed google maps API integration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## React Weather# React-Weather-App
+
+### A React project created for practice in API/AJAX on React.js
--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
+
+import { getCurrentLatLng } from './services/geolocation';
+
 import './App.css';
 import Map from './components/Map/Map';
 
 function App () {
+
+  const [appData, setAppData] = useState({
+    lat: null,
+    lng: null,
+  })
+
+  const getAppData = async () => {
+    // browser location 
+    const {lat, lng} = await getCurrentLatLng();
+    // weather data
+    // set component state to that data
+    setAppData({lat, lng});
+  }
+
+  useEffect(() => {
+    getAppData();
+  }, [])
+
   return (
     <div className='App'>
-      <Map />
+      <Map lat={appData.lat} lng={appData.lng}/>
       <header className='App-header'>REACT WEATHER</header>
     </div>
   );
